8532a747413dddcc987e9a76f92ac9ae94f8a5f5
Oguzhan Onal <oguzhanonal52@hotmail.com>
2024-12-18 12:27:43 +0300

    fix: deleted proxy

--- server/middleware/proxy.js
+++
@@ -1,5 +0,0 @@
-// export default defineEventHandler(async (event) => {
-//   if (event.node.req.url.startsWith("/api")) {
-//     return proxyRequest(event, "https://kostbuddy.netlify.app/");
-//   }
-// });
